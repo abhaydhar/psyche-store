b16fbe8e7209d3355e539a674e5a40d86d971ad7
abhay_dhar@epam.com <abhay_dhar@epam.com>
2026-08-31 12:22:13 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb', // Set body size limit to 10MB
-    },
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
